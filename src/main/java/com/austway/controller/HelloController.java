759f8b7a265c140edddca02ccea50f284f001005
vidorchan <1282911204@qq.com>
2018-06-05 20:53:11 +0800

    first commit,hello demo

---
+++ src/main/java/com/austway/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.austway.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value="/hello")
+    public String hello() {
+        return "hello";
+    }
+
+}
